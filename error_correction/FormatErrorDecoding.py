c6083dab06ddf43e43392d2c97ff2c4a0d53dca1
Basak <chgy2131@naver.com>
2023-01-10 18:16:34 +0900

    [feat] nmea format error일 때 마지막 필드가 checksum인 경우 제거하는 기능 추가

--- error_correction/FormatErrorDecoding.py
+++ error_correction/FormatErrorDecoding.py
@@ -1,5 +1,7 @@
 from pyais import decode as ais_decode
 from pyais import util
+import re
+
 class FormatDecoder:
     def decode(self, raw_data):
         last_field = self.pick_last_field(raw_data)
@@ -8,9 +10,16 @@ class FormatDecoder:
         return util.decode_into_bit_array(binary_raw_data)
 
     def decode_with_checksum(self, raw_data):
-        # util.decode_into_bit_array()
-        return raw_data
+        last_field = self.pick_last_field(raw_data)
+        binary_raw_data = bytes(last_field, 'utf-8')
+
+        return util.decode_into_bit_array(binary_raw_data)
 
     def pick_last_field(self, raw_data):
+        # 체크섬이 존재하는 경우 삭제
+        raw_data = re.sub('\d\*\d\d$', '', raw_data)
+        raw_data.rstrip(',')
+
+        # 마지막 필드를 가져온다
         r = raw_data.split(',')
         return r[len(r) - 1].strip()
